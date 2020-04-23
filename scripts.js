a77fa8de9e1dde4f8c599f05c13462187a975018
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-04-23 13:19:59 -0400

    event click iterate over button nodelist to update function

--- scripts.js
+++ scripts.js
@@ -8,21 +8,36 @@ const divide = (...nums) => nums.reduce((tot, num) => tot / num);
 
 const operate = (operator, num1, num2) => operator(num1, num2);
 
-
-
+let currentNum, num1, num2, operator;
 const operation = document.querySelector('#operation');
 const screen = document.querySelector('#screen');
-let display = screen.textContent;
 const buttons = document.querySelectorAll('button');
-
-
 
 const operators = document.getElementsByClassName('operators');
 
 
-//document.addEventListener('click', updateDisplay);
+buttons.forEach((button) => {
+    button.addEventListener('click', update);
+});
 
-function updateDisplay(value) {
-    screen.textContent += value;
-} 
+
+function update() {
+    if (this.parentElement.classList.contains('digits')) {
+        screen.textContent += this.innerHTML;
+        currentNum += this.textContent; // combine these?
+
+    } else if (this.parentElement.classList.contains('operators')) {
+        operator = this.value;
+        (num1) ? num2 = currentNum : num1 = currentNum;
+        currentNum = 0;
+        screen.textContent += this.textContent;
+
+    } else if (this.parentElement.classList.contains('operations')) {
+        if (this.value == 'clear') {
+            //clear(); //make clear fxn, clear screen and reset variables num/ops
+        } else if (this.value == 'execute') {
+            //execute(); //make fxn, operate, clear after x time?
+        }   
+    }
+}    
 
