228ced9b0179dac139b9488d183b94e12aef5e97
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-04-24 10:38:06 -0400

    calculator fully functional

--- scripts.js
+++ scripts.js
@@ -1,25 +1,56 @@
-const add = (...nums) => nums.reduce((tot, num) => tot + num);
+function add(a, b) {
+    return a + b;
+};
 
-const subtract = (...nums) => nums.reduce((tot, num) => tot - num);
+function subtract(a, b) {
+    return a - b;
+};
 
-const multiply = (...nums) => nums.reduce((tot, num) => tot * num);
+function multiply(a, b) {
+    return a * b;
+};
 
-const divide = (...nums) => nums.reduce((tot, num) => tot / num);
+function divide(a, b) {
+    if (b==0) {
+        brokeSystem();
+        return 'nyet';
+    } else {
+        return (a / b);
+    }
+}
 
-const operate = (operator, num1, num2) => operator(num1, num2);
+function operate(operator, num1, num2) {
+    return window[operator](num1, num2);
+};
 
-let currentNum, num1, num2, operator;
+
+let currentNum =0, num1=0, num2=0, operator;
 const operation = document.querySelector('#operation');
 const screen = document.querySelector('#screen');
 const buttons = document.querySelectorAll('button');
-
-const operators = document.getElementsByClassName('operators');
+const message = document.querySelector('.message');
 
 
 buttons.forEach((button) => {
     button.addEventListener('click', update);
 });
 
+function execute() {
+    if (num1 && num2 && operator) {
+        let result = operate(operator, num1, num2).toFixed(5).replace(/0+$/, "");
+        screen.textContent = result;
+        num1 = result;
+        num2 = 0;
+    }
+}
+
+function clear() {
+    currentNum = 0;
+    num1 = 0;
+    num2 = 0;
+    operator = '';
+    screen.textContent = '';
+};
 
 function update() {
     if (this.parentElement.classList.contains('digits')) {
@@ -27,17 +58,33 @@ function update() {
         currentNum += this.textContent; // combine these?
 
     } else if (this.parentElement.classList.contains('operators')) {
+        updateNums();
         operator = this.value;
-        (num1) ? num2 = currentNum : num1 = currentNum;
-        currentNum = 0;
         screen.textContent += this.textContent;
-
+        
     } else if (this.parentElement.classList.contains('operations')) {
+        
         if (this.value == 'clear') {
-            //clear(); //make clear fxn, clear screen and reset variables num/ops
+            clear(); //make clear fxn, clear screen and reset variables num/ops
         } else if (this.value == 'execute') {
-            //execute(); //make fxn, operate, clear after x time?
-        }   
+            updateNums();
+            execute(); //make fxn, operate, clear after x time?
+        }
     }
 }    
 
+function updateNums() {
+    if (!(num1)) {
+        num1 = currentNum;
+     } else {
+        num2 = currentNum;
+     }
+    currentNum = 0;
+}
+
+function brokeSystem() {
+    error = document.createElement('h2');
+    error.textContent = 'You broke the system';
+    error.style.color = 'red';
+    message.appendChild(error);
+}
