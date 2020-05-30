febf5fa9ee4bbd3c479c39ca4a4beb6998844b78
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-05-30 19:58:57 -0400

    Updates

---
+++ precache-manifest.cd5d2b4d30ca380af18770a00a4b1e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addfcabec232b0bab5bca8ab0bc9f47b",
+    "url": "/nima-m-git/react_demo.git/index.html"
+  },
+  {
+    "revision": "b4cb4536a3d5cba41a24",
+    "url": "/nima-m-git/react_demo.git/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "dc25104e5ea14dd734b9",
+    "url": "/nima-m-git/react_demo.git/static/js/2.8b267e20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nima-m-git/react_demo.git/static/js/2.8b267e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cb4536a3d5cba41a24",
+    "url": "/nima-m-git/react_demo.git/static/js/main.46e6e251.chunk.js"
+  },
+  {
+    "revision": "f99867616e267acd769e",
+    "url": "/nima-m-git/react_demo.git/static/js/runtime-main.3a914dc5.js"
+  }
+]);
